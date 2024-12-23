8fa9502168b782db41eaecd2560044a891766500
Lucas Valdez <lnvaldez24@gmail.com>
2024-12-23 11:42:53 -0300

    feat: hash and comparing for user model

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema(
   {
@@ -8,5 +9,15 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.comparePassword = async function (requestPassword) {
+  return bcrypt.compare(requestPassword, this.password);
+};
 
 module.exports = mongoose.model("User", userSchema);
