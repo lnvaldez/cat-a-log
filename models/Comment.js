b9b706b41306406e6b07e5ac5600ac828c87c396
Lucas Valdez <lnvaldez24@gmail.com>
2024-12-23 11:39:01 -0300

    refactor: createdBy field instead of user

--- models/Comment.js
+++ models/Comment.js
@@ -1,10 +1,16 @@
 const mongoose = require("mognoose");
 
-const commentSchema = new mongoose.Schema({
-  text: { type: String, required: true },
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  link: { type: mongoose.Schema.Types.ObjectId, ref: "Link", required: true },
-  createdAt: { type: Date, default: Date.now },
-});
+const commentSchema = new mongoose.Schema(
+  {
+    text: { type: String, required: true },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    link: { type: mongoose.Schema.Types.ObjectId, ref: "Link", required: true },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Comment", commentSchema);
--- models/Link.js
+++ models/Link.js
@@ -1,13 +1,19 @@
 const mongoose = require("mongoose");
 
-const linkSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  url: { type: String, required: true },
-  description: { type: String },
-  tags: [{ type: String }],
-  votes: { type: Number, default: 0 },
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  createdAt: { type: Date, default: Date.now },
-});
+const linkSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    url: { type: String, required: true },
+    description: { type: String },
+    tags: [{ type: String }],
+    votes: { type: Number, default: 0 },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Link", linkSchema);
