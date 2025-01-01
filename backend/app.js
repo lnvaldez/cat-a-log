c0db76ee740f850c75a82c1ec007e7720d90d2a5
Lucas Valdez <lnvaldez24@gmail.com>
2025-01-01 12:41:47 -0300

    feat: frontend-angular

--- backend/app.js
+++ backend/app.js
@@ -26,6 +26,7 @@ app.use(
       "http://localhost:3000",
       "http://localhost:5173",
       "http://localhost:5174",
+      "http://localhost:4200",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
---
+++ frontend-angular/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  standalone: true,
+  imports: [RouterModule, CommonModule],
+})
+export class AppComponent {
+  title = 'Link Library';
+}
